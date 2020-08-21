6c4fbad77a4a39a5b6bf20c3e06ad6f9f5c4dec3
Sanny Do <sanny.do94@gmail.com>
2020-08-21 03:15:46 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,13 +13,10 @@ end
 
 def selection(num)
   if num == 1
- expect(selection(1)).to eq("YUM YUM MUNCH MUNCH MUNCH")
      puts "YUM YUM MUNCH MUNCH MUNCH"
   elsif num == 2
-  expect(selection(2)).to eq("HAM HAM HAM IN MY TUMMY")
      puts "HAM HAM HAM IN MY TUMMY"
   end
-  return selection(num(1))
 end
 
 def runner
