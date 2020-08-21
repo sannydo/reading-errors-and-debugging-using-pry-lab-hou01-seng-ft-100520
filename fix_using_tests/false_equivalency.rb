4c6c319b15d904357218afe756a03376d95d4774
Sanny Do <sanny.do94@gmail.com>
2020-08-21 03:18:55 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,9 +13,9 @@ end
 
 def selection(num)
   if num == 1
-     puts "YUM YUM MUNCH MUNCH MUNCH"
+     return "YUM YUM MUNCH MUNCH MUNCH"
   elsif num == 2
-     puts "HAM HAM HAM IN MY TUMMY"
+     return "HAM HAM HAM IN MY TUMMY"
   end
 end
 
