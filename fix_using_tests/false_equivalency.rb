09b9f5d288e6c7a5d897fd1e37874b3d84901a11
Sanny Do <sanny.do94@gmail.com>
2020-08-21 02:58:58 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,5 +1,6 @@
 # don't forget to add: require 'pry'
 
+
 def get_user_input
   gets.chomp
 end
@@ -11,14 +12,18 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+  if num == 1
+ expect(selection(1)).to eq("YUM YUM MUNCH MUNCH MUNCH")
+     puts "YUM YUM MUNCH MUNCH MUNCH"
+  elsif num == 2
+  expect(selection(2)).to eq("HAM HAM HAM IN MY TUMMY")
+     puts "HAM HAM HAM IN MY TUMMY"
   end
+  return selection(num(1))
 end
 
 def runner
-  prompt_user
+  prompt_user 
   selection(get_user_input)
-end
+ end
+
